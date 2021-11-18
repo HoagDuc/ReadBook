4d0327b1af82700a9da3e0e0618563c869ac38a7
HoagDuc <hoangduc3672@gmail.com>
2021-11-18 21:54:51 +0700

    up

--- app/src/main/java/com/nvb/appsach/MainActivity.java
+++ app/src/main/java/com/nvb/appsach/MainActivity.java
@@ -26,8 +26,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.google.android.material.navigation.NavigationView;
+import com.huawei.hms.ads.AdParam;
+import com.huawei.hms.ads.BannerAdSize;
+import com.huawei.hms.ads.HwAds;
+import com.huawei.hms.ads.banner.BannerView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -36,6 +41,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Initialize the HUAWEI Ads SDK.
+        HwAds.init(this);
+
+        // Obtain BannerView configured in the XML layout file.
+        BannerView bottomBannerView = findViewById(R.id.hw_banner_view);
+        AdParam adParam = new AdParam.Builder().build();
+        bottomBannerView.loadAd(adParam);
+
+        // Add BannerView through coding.
+        BannerView topBannerView = new BannerView(this);
+        topBannerView.setAdId("testw6vs28auh3");
+        topBannerView.setBannerAdSize(BannerAdSize.BANNER_SIZE_SMART);
+        topBannerView.loadAd(adParam);
+
+        RelativeLayout rootView = findViewById(R.id.root_view);
+        rootView.addView(topBannerView);
         drawerLayout = findViewById(R.id.nav_view);
         Toolbar toolbar = findViewById(R.id.toolBar);
         NavigationView navigationView = findViewById(R.id.navigation_view);
